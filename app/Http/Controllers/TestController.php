4a2d0a0eaaac7e5ac4b99c91c50098c7809da5f7
Oxfard <oxfard@mail.ru>
2025-05-03 16:48:43 +0700

    Добавлен тестовый контроллер

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        return 'message 7';
+    }
+}
